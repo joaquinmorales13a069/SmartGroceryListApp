c146fb90b2b74cad6284b675105586519a1b4c7c
Joaquin Morales <joaquinmorales13a06@gmail.com>
2025-06-23 19:41:30 +1000

    Adds dependencies and server setup for React and MongoDB
    Integrates new dependencies including React-related libraries (`axios`, `react-cookie`, `react-router-dom`, `react-toastify`) and server-side libraries (`bcrypt`, `cookie-parser`, `cors`, `dotenv`, `jsonwebtoken`, `mongoose`) for enhanced functionality.
    
    Removes unused CSS files and refactors `App.jsx` for simpler structure. Introduces a server configuration with Express and MongoDB connection, including CORS and environment variable support.
    
    Supports improved client-server communication and streamlined project setup.

---
+++ Server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./middleware/connectDB.js";
+
+const app = express();
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Middleware for CORS
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware and routes
+app.use(express.json()); // Parse JSON bodies
+
+
+// Launch the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
